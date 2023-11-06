2e0d10389aa475f1b8a1582cbb87ac7ab173adfb
Emilia Lejonstig <emilia.lejonstig@gmail.com>
2023-11-06 11:15:50 +0100

    first commit

---
+++ code/pizzabot.js
@@ -0,0 +1,21 @@
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = " Hawaiian Pizza";
+const pepperoni = " Pepperoni Pizza";
+
+const pizzaPrice = 80;
+
+//Put your Javscript code here:
+
+alert(
+  "Hey! Happy to serve your pizza. On our menu we have " +
+    vegetarian +
+    hawaiian +
+    " and" +
+    pepperoni
+);
+
+const orderName = prompt(
+  "Enter the name of the pizza you want to order today."
+);
+
+console.log(orderName);
